d455814c5f980be3f76f7487eeec3de9cee01353
Ketki Kule <kule.k@northeastern.edu>
2022-03-10 02:10:16 -0500

    RDS db connection

--- _helpers/db.js
+++ _helpers/db.js
@@ -10,8 +10,12 @@ initialize();
 
 async function initialize() {
     // create db if it doesn't already exist
-    const { host, port, user, password, database } = config.database;
+   // const { host, port, user, password, database } = config.database;
     //const connection = await mysql.createConnection({ host, port, user, password });
+    console.log(process.env.DB_HOST);
+    console.log(process.env.DB_USERNAME);
+    console.log(process.env.DB_PASSWORD);
+    console.log(process.env.DB_PORT);
     const connection = await mysql.createConnection({ 
         host: process.env.DB_HOST, 
         user: process.env.DB_USERNAME,
@@ -21,7 +25,25 @@ async function initialize() {
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_DATABASE}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, { dialect: 'mysql' });
+    const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+        host : process.env.DB_HOST,
+        dialectOptions: {
+            ssl: 'Amazon RDS'
+        },
+        dialect : 'mysql',
+        replication:{
+            read :[
+                {host: process.env.DB_HOST, username: process.env.DB_USERNAME, password:process.env.DB_PASSWORD}
+            ],
+            write : {host : process.env.DB_HOST,username:process.env.DB_USERNAME, password:process.env.DB_PASSWORD}
+        },
+        pool : {
+            max : 5,
+            min : 0,
+            acquire : 30000,
+            idle : 10000 
+        }
+    });
 
     // init models and add them to the exported db object
     db.User = require('../users/user.model')(sequelize);
